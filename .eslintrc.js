3cab4ced9235610354cfaf2d21ee490c3d27b400
Albert-cord <fjr_studio@163.com>
2019-06-02 14:35:57 +0800

    feat:新增test模块karma，新增eslint规范代码

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "amd": true
+  },
+  "rules": {
+    "indent": [
+      "warn",
+      4
+    ]
+  }
+}
---
+++ test/array.js
@@ -0,0 +1,4 @@
+(function() {
+    var _ = typeof require === 'function' ? require('..') : window._;
+    // to do:learn how to use karma and qunit
+})()
